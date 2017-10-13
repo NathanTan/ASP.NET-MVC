28a6caabad1fbd2359622863177ab978cbe2d0be
NathanTan <tann@oregonstate.edu>
2017-10-13 10:46:29 -0700

    Inital commit.

---
+++ MVC Demo/MVC Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Demo.Startup))]
+namespace MVC_Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
