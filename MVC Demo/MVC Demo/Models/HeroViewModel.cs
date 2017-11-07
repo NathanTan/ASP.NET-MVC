b2e5bd0795afbdf5d5d110368b402ee30169b35e
tann <tann@CASS-DEV02>
2017-11-07 11:23:57 -0800

    Change start point to have ViewModel

---
+++ MVC Demo/MVC Demo/Models/HeroViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Demo.Models
+{
+    public class HeroViewModel
+    {
+        public string Name { get; set; }
+        public string HeroName { get; set; }
+        public string SidekickName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string OriginCity { get; set; }
+        public string Region { get; set; }
+        public IEnumerable<SelectListItem> PossibleRegions { get; set; }
+    }
+}
