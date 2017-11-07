7e4a0cc02c60fe842c82acb0e28c23dc71901158
tann <tann@CASS-DEV02>
2017-11-07 14:26:56 -0800

    Clean up for start.

--- MVC Demo/MVC Demo/Models/HeroViewModel.cs
+++ MVC Demo/MVC Demo/Models/HeroViewModel.cs
@@ -11,10 +11,10 @@ namespace MVC_Demo.Models
     {
         public string Name { get; set; }
         public string HeroName { get; set; }
+        public string OriginCity { get; set; }
         public string SidekickName { get; set; }
         public string Email { get; set; }
         public int Age { get; set; }
-        public string OriginCity { get; set; }
         public string Region { get; set; }
         public IEnumerable<SelectListItem> PossibleRegions { get; set; }
     }
