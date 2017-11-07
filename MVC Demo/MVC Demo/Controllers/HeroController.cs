31067a2ff5b6cdf1041f2382d2d49e66d00a05a6
tann <tann@CASS-DEV02>
2017-11-07 13:56:01 -0800

    Start with controller

---
+++ MVC Demo/MVC Demo/Controllers/HeroController.cs
@@ -0,0 +1,26 @@
+﻿using MVC_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Demo.Controllers
+{
+    public class HeroController : Controller
+    {
+        // GET: Hero
+        [HttpGet]
+        public ActionResult Index()
+        {
+            HeroViewModel model = new HeroViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(HeroViewModel data)
+        {
+            return View(data);
+        }
+    }
+}
